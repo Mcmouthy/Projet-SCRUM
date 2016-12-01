d72f3c164ca7d9dd2d0f65758bb93fe4efcd341a
malonesk <jeremy.tran@edu.univ-fcomte.fr>
2016-12-01 20:50:26 +0100

    suppression des conneries

--- Projet_Scrum/Balai/Acrobatie.java
+++
@@ -1,8 +0,0 @@
-package Balai;
-
-/***
- * Created by Sami on 22/11/2016.
- */
-public class Acrobatie extends CarteMagieNoire {
-    public Acrobatie(){}
-}
--- Projet_Scrum/Balai/Sortilege.java
+++
@@ -1,7 +0,0 @@
-package Balai;
-
-/**
- * Created by Sami on 22/11/2016.
- */
-public class Sortilege extends CarteMagieNoire {
-}
